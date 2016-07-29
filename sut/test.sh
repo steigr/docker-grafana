98fde10cb3ed90eb97ed339628d9e2f7fbc81901
Nicolas Degory <ndegory@axway.com>
2016-07-29 19:47:05 +0000

    new test for ci

--- sut/test.sh
+++ sut/test.sh
@@ -4,6 +4,16 @@ GRAFANA_HOST=${GRAFANA_HOST:-grafana}
 GRAFANA_USER=admin
 GRAFANA_PASS=changeme
 
+echo -n "test grafana presence...   "
+nslookup grafana >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  echo
+  echo "failed"
+  nslookup grafana
+  exit 1
+fi
+printf "%-16s[OK]\n" "resolves"
+
 echo -n "test grafana login page... "
 i=0
 r=1
@@ -12,7 +22,7 @@ while [[ $r -ne 0 ]]; do
   sleep 1
   curl -L $GRAFANA_HOST:3000 2>/dev/null | grep -q '<title>Grafana</title>'
   r=$?
-  if [[ $i -gt 30 ]]; then break; fi
+  if [[ $i -gt 40 ]]; then break; fi
 done
 if [[ $r -ne 0 ]]; then
   echo
@@ -20,7 +30,7 @@ if [[ $r -ne 0 ]]; then
   curl -L $GRAFANA_HOST:3000
   exit 1
 fi
-printf "%-12s[OK]\n" "ready"
+printf "%-16s[OK]\n" "ready (${i}s)"
 
 echo -n "test grafana auth...       "
 org=$(curl -u $GRAFANA_USER:$GRAFANA_PASS $GRAFANA_HOST:3000/api/org 2>/dev/null | jq -r '.name')
@@ -31,7 +41,7 @@ if [[ $r -ne 0 || -z "$org" || "x$org" = "xnull" ]]; then
   curl -u $GRAFANA_USER:$GRAFANA_PASS $GRAFANA_HOST:3000/api/org 2>/dev/null
   exit 1
 fi
-printf "%-12s[OK]\n" "($org)"
+printf "%-16s[OK]\n" "($org)"
 
 echo -n "test grafana datasource... "
 ds=$(curl -u $GRAFANA_USER:$GRAFANA_PASS $GRAFANA_HOST:3000/api/datasources/name/telegraf 2>/dev/null | jq -r '.name')
@@ -41,7 +51,7 @@ if [[ $r -ne 0 || "x$ds" != "xtelegraf" ]]; then
   echo "failed to found datasource"
   exit 1
 fi
-printf "%-12s[OK]\n" "($ds)"
+printf "%-16s[OK]\n" "($ds)"
 
 echo -n "test grafana dashboards... "
 n=$(curl -u $GRAFANA_USER:$GRAFANA_PASS $GRAFANA_HOST:3000/api/search?query=AMP%20%Swarm%20Health 2>/dev/null | jq -r 'map(select(.type=="dash-db"))| length')
@@ -51,5 +61,5 @@ if [[ $r -ne 0 || -z "$n" || "x$n" = "xnull" || $n -lt 1 ]]; then
   echo "failed to found dashboards"
   exit 1
 fi
-printf "%-12s[OK]\n" "($n)"
+printf "%-16s[OK]\n" "($n)"
 echo "all tests passed successfully"
